0383ef7409d1ffa60cbb6896abe30197048c1854
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-05-24 21:29:01 +0700

    Delete show-history.js

--- show-history.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const history = JSON.parse(localStorage.getItem("history")) || [];
-  const countEl = document.getElementById("user-count");
-  countEl.textContent = history.length;
-});
