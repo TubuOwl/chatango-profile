7fd8f0aa17a885ff961a0d85c4683e52127ea244
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-05-24 20:14:20 +0700

    Update visit.js

--- js/visit.js
+++ js/visit.js
@@ -1,1 +1,20 @@
+window.onload = start;
 
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function start() {
+  let name;
+  let cookie = document.cookie;
+
+  try {
+      let found = cookie.match("id.chatango.com=(.*?);");
+      let user = found ? found[1] : null;
+      name = user.charAt(0).toUpperCase() + user.slice(1);
+    } catch {
+      name = "Anon" + rand(1000, 9999);
+    }
+
+    document.getElementById("user").textContent = name;
+  }
