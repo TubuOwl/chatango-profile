7108a1feb1f886ae1bdbc6acad9c4a45b7229cf0
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-05-24 19:47:20 +0700

    Create visit.js

---
+++ js/visit.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  let name;
+  let cookie = document.cookie;
+
+  try {
+    let found = cookie.match(/id\.chatango\.com=([^;]+)/);
+    let user = found ? found[1] : null;
+    name = user.charAt(0).toUpperCase() + user.slice(1);
+  } catch {
+    name = "Anon" + Math.floor(Math.random() * 9000 + 1000);
+  }
+
+  document.getElementById("user").textContent = name;
