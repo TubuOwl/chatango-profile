a3d5c2981f437cac67f668a7dc3f7816c73e2a8a
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-05-29 21:36:16 +0700

    Update visit.js

--- js/visit.js
+++ js/visit.js
@@ -1,4 +1,3 @@
-// Fungsi untuk mengambil cookies sebagai objek
 function getCookies() {
   const pairs = document.cookie.split(";");
   const cookies = {};
@@ -9,24 +8,19 @@ function getCookies() {
   return cookies;
 }
 
-// Variabel untuk menyimpan cookies terakhir
 let lastCookies = {};
 
-// Fungsi untuk memeriksa perubahan cookies
 function checkCookies() {
   const currentCookies = getCookies();
 
-  // Bandingkan dengan cookies terakhir yang disimpan
   const changed = JSON.stringify(currentCookies) !== JSON.stringify(lastCookies);
 
   if (changed) {
     console.log("Ada perubahan cookie:", currentCookies);
-    lastCookies = currentCookies; // update cookies terakhir
+    lastCookies = currentCookies;
   }
 }
 
-// Ambil cookies pertama kali saat halaman dimuat
 lastCookies = getCookies();
 
-// Jalankan pengecekan setiap 5 detik (5000 ms)
 setInterval(checkCookies, 5000);
