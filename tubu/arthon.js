932ef9e80132f6dabd152eb311dac25acd1c7997
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-09-08 21:30:41 +0700

    Update arthon.js

--- tubu/arthon.js
+++ tubu/arthon.js
@@ -1,7 +1,5 @@
 const music = document.getElementById("bg-music");
 const video = document.getElementById("bgVideo");
-
-const video = document.getElementById("bgVideo");
 const sources = [
       "https://www.desktophut.com/files/pXJv4LBHTS-l4d2background01.mp4",
       "https://www.desktophut.com/files/o1TAlp3047-l4d2background02.mp4",
@@ -10,15 +8,15 @@ const sources = [
       "https://www.desktophut.com/files/neAC2K9PM3-l4d2background05.mp4"
     ];
 let index = 0;
-setInterval(() => {
-  video.classList.add("fade-out");
-  setTimeout(() => {
-    index = (index + 1) % sources.length;
-    video.src = sources[index];
-    video.play();
-    video.classList.remove("fade-out");
-  }, 0);
-}, 10000);
+    setInterval(() => {
+      video.classList.add("fade-out");
+      setTimeout(() => {
+        index = (index + 1) % sources.length;
+        video.src = sources[index];
+        video.play();
+        video.classList.remove("fade-out");
+      }, 0);
+    }, 10000);
 function enableMusic() {
   music.muted = false;
   music.play().catch(err => console.log("Autoplay blocked:", err));
