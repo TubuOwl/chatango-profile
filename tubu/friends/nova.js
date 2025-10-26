6176806815879c7b1c46d1c73e5f76ece5c44f95
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-10-26 22:36:29 +0700

    Create nova.js

---
+++ tubu/friends/nova.js
@@ -0,0 +1,15 @@
+
+
+<script>
+  function showFriendWindow() {
+    document.getElementById("friendWindow").style.display = "block";
+    document.getElementById("openBtn").style.display = "none";
+  }
+  function hideFriendWindow() {
+    document.getElementById("friendWindow").style.display = "none";
+    document.getElementById("openBtn").style.display = "block";
+  }
+</script>
+
+</body>
+</html>
